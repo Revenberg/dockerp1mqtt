6dd2fb1a569fe8bc3edb5be71247a8af0ef1e57c
Sander Revenberg <sander@revenberg.info>
2021-11-08 07:18:49 +0100

    .

--- files/app/p1logger.py
+++ files/app/p1logger.py
@@ -11,10 +11,18 @@ import time
 import json
 import random
 import time
-import settings
 
 mqttclientid = f'python-mqtt-{random.randint(0, 1000)}'
 crc16 = crcmod.predefined.mkPredefinedCrcFun('crc16')
+
+do_raw_log = bool(os.getenv("LOGGING", "false"))
+
+device = os.getenv("P1_DEVICE", "/dev/ttyUSB0")
+baudrate = int(os.getenv("P1_BAUDRATE", "115200"))
+
+mqttBroker = os.getenv("MQTT_ADDRESS", "192.168.2.59")
+mqttPort = int(os.getenv("MQTT_PORT", "1883"))
+mqttTopic = os.getenv("MQTT_TOPIC", "p1")
 
 values = dict()
 
@@ -249,4 +257,4 @@ def connect_mqtt(mqttclientid, mqttBroker, mqttPort ):
 
 client=connect_mqtt(mqttclientid, mqttBroker, mqttPort )
 client.loop_start()
-getData(client, mqtttopic, device, baudrate)
+getData(client, mqttTopic, device, baudrate)
